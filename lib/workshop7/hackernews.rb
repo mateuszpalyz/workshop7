685b194cae080258d79a2f0322d42a164eb07a53
Mateusz Palyz <m.palyz@pilot.co>
2014-12-23 18:52:44 +0100

    #2 make response body respond to each

--- lib/workshop7/hackernews.rb
+++ lib/workshop7/hackernews.rb
@@ -2,7 +2,7 @@ module Workshop7
 
   class HackerNews
     def call(env)
-      [200, {"Content-Type" => "text/html"}, "Hacker News"]
+      [200, {"Content-Type" => "text/html"}, ["Hacker News"]]
     end
   end
 end
