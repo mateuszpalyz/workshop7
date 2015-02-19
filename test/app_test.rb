0cc8dfc9e3a9a3fc4caffabd6027249bd991bf1c
Mateusz Palyz <m.palyz@pilot.co>
2015-02-19 13:07:28 +0100

    #3 add api tests

--- test/app_test.rb
+++ test/app_test.rb
@@ -11,11 +11,65 @@ class AppTest < Minitest::Test
     Rack::Lint.new(HackerNews.new)
   end
 
-  def setup
+  def test_response
     get '/'
+
+    assert last_response.ok?
   end
 
-  def test_response
+  def test_getting_list_of_all_submitted_stories
+    skip
+    get '/stories'
+
     assert last_response.ok?
+  end
+
+  def test_getting_a_single_story
+    skip
+    get '/stories/1'
+
+    assert last_response.ok?
+  end
+
+  def test_submitting_a_new_story
+    skip
+    post '/stories', {}
+
+    assert_equal 201, last_response.status
+  end
+
+  def test_updating_a_story
+    skip
+    patch '/stories/1', {}
+
+    assert last_response.ok?
+  end
+
+  def test_upvoting_a_story
+    skip
+    put '/stories/1/votes', {}
+
+    assert_equal 201, last_response.status
+  end
+
+  def test_downvoting_a_story
+    skip
+    put '/stories/1/votes', {}
+
+    assert_equal 201, last_response.status
+  end
+
+  def test_undoing_a_vote
+    skip
+    delete '/stories/1/votes'
+
+    assert_equal 204, last_response.status
+  end
+
+  def test_creating_a_user
+    skip
+    post '/users', {}
+
+    assert_equal 201, last_response.status
   end
 end
