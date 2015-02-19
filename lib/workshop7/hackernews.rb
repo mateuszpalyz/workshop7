1c1355dd7cab0355cb9504df656f11a6c91fc904
Mateusz Palyz <m.palyz@pilot.co>
2015-02-19 14:11:27 +0100

    #4 return json for stories and single story

--- lib/workshop7/hackernews.rb
+++ lib/workshop7/hackernews.rb
@@ -1,8 +1,35 @@
 module Workshop7
+  require 'sinatra'
+  require 'json'
 
-  class HackerNews
-    def call(env)
-      [200, {"Content-Type" => "text/html"}, ["Hacker News"]]
+  class HackerNews < Sinatra::Base
+    get '/' do
+      'Sinatra !!!'
+    end
+
+    get '/stories' do
+      content_type :json
+      [
+        {
+          id: 1,
+          title: 'Lorem ipsum',
+          url: 'http://www.lipsum.com/',
+        },
+        {
+          id: 2,
+          title: 'Ipsum Lorem',
+          url: 'http://www.ipsum.com',
+        },
+      ].to_json
+    end
+
+    get '/stories/:id' do
+      content_type :json
+      {
+        id: 1,
+        title: 'Lorem ipsum',
+        url: 'http://www.lipsum.com/',
+      }.to_json
     end
   end
 end
