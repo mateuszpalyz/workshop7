3a4feda66c21a535ee7d03468e484db4a6e4b1b7
Mateusz Palyz <m.palyz@pilot.co>
2015-02-20 12:58:30 +0100

    #5 connect sinatra app to the db

--- lib/workshop7/hackernews.rb
+++ lib/workshop7/hackernews.rb
@@ -1,8 +1,15 @@
 module Workshop7
   require 'sinatra'
   require 'json'
+  require 'yaml'
+  require 'active_record'
 
   class HackerNews < Sinatra::Base
+    configure do
+      db_options = YAML.load_file('config/database.yml')[environment.to_s]
+      ActiveRecord::Base.establish_connection(db_options)
+    end
+
     get '/' do
       'Sinatra !!!'
     end
