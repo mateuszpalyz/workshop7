cc02474d802889a4448e9996b9ccb38ddb7a536f
Mateusz Palyz <m.palyz@pilot.co>
2015-02-20 21:50:40 +0100

    #2.1 allow to post new stories

--- lib/workshop7/hackernews.rb
+++ lib/workshop7/hackernews.rb
@@ -22,5 +22,17 @@ module Workshop7
     get '/stories/:id' do
       Story.find(params[:id]).to_json
     end
+
+    post '/stories' do
+      story = Story.create(title: params[:title], url: params[:url])
+
+      status 201
+      content_type :json
+        {
+          id: story.id,
+          title: story.title,
+          url: story.url,
+        }.to_json
+    end
   end
 end
