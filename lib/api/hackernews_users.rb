48bed60d3e2667219f8ac039023619b0237332c1
Mateusz Palyz <m.palyz@pilot.co>
2015-03-03 14:33:47 +0100

    #2.2 allow creating new users

--- lib/api/hackernews_users.rb
+++ lib/api/hackernews_users.rb
@@ -2,24 +2,14 @@ module Workshop7
   require 'api/hackernews_base'
 
   class HackerNewsUsers < HackerNewsBase
-    get '/stories' do
-      content_type :json
-      Story.all.to_json
-    end
-
-    get '/stories/:id' do
-      Story.find(params[:id]).to_json
-    end
-
-    post '/stories' do
-      story = Story.create(title: params[:title], url: params[:url])
+    post '/users' do
+      user = User.create(username: params[:username], password: params[:password])
 
       status 201
       content_type :json
         {
-          id: story.id,
-          title: story.title,
-          url: story.url,
+          id: user.id,
+          username: user.username,
         }.to_json
     end
   end
