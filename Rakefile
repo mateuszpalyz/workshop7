9b3cc4a2c7a167ca88d4c0ae2180f72f20bc6946
Mateusz Palyz <m.palyz@pilot.co>
2015-03-05 13:44:53 +0100

    #2.10 connect to the pg db

--- Rakefile
+++ Rakefile
@@ -17,7 +17,7 @@ namespace :db do
   end
 
   task :connection => :environment do
-    db_options = YAML.load_file('config/database.yml')[ENVIRONMENT]
+    db_options = ENV['DATABASE_URL'] ? ENV['DATABASE_URL'] : YAML.load_file('config/database.yml')[environment.to_s]
     ActiveRecord::Base.establish_connection(db_options)
   end
 
--- lib/api/hackernews_base.rb
+++ lib/api/hackernews_base.rb
@@ -12,7 +12,7 @@ module Workshop7
     include Workshop7::Helpers
 
     configure do
-      db_options = YAML.load_file('config/database.yml')[environment.to_s]
+      db_options = ENV['DATABASE_URL'] ? ENV['DATABASE_URL'] : YAML.load_file('config/database.yml')[environment.to_s]
       ActiveRecord::Base.establish_connection(db_options)
     end
   end
