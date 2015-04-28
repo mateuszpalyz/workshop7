5db3163ebae00bc8043983933bf4249815ed779a
Mateusz Palyz <m.palyz@pilot.co>
2015-04-28 15:14:44 +0200

    #2.4 #2.5 #2.6 use error handler for votes creation and deletion

--- lib/api/hackernews_stories.rb
+++ lib/api/hackernews_stories.rb
@@ -2,6 +2,8 @@ module Workshop7
   require 'api/hackernews_base'
 
   class HackerNewsStories < HackerNewsBase
+    set :show_exceptions, :after_handler
+
     get '/stories' do
       content_type format
       convert_to_correct_format(Story.all)
@@ -49,20 +51,13 @@ module Workshop7
       protected!
 
       point = (JSON.parse request.body.read)['point']
-      vote = Vote.find_or_initialize_by(user_id: @user.id, story_id: params[:id], point: point)
+      vote = Vote.find_or_initialize_by(user_id: @user.id, story_id: params[:id])
+      vote.point = point
+      vote.save!
 
-      if vote.save
-        status 201
-        content_type format
-        data = {
-            points: Story.find(params[:id]).points
-          }
-        convert_to_correct_format(data)
-      else
-        status 422
-        content_type format
-        convert_to_correct_format(vote.errors)
-      end
+      status 201
+      content_type format
+      convert_to_correct_format({points: Story.find(params[:id]).points})
     end
 
     delete '/stories/:id/vote' do
@@ -76,6 +71,16 @@ module Workshop7
 
     get '/stories/:id/url' do
       redirect Story.find(params[:id]).url, 303
+    end
+
+    error ActiveRecord::RecordInvalid do |invalid|
+      status 422
+      content_type format
+      convert_to_correct_format(invalid.record.errors)
+    end
+
+    error ActiveRecord::RecordNotFound do
+      status 422
     end
   end
 end
